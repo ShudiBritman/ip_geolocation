21305c8920a2a39b8a45da054a5f9b19fabe4c8f
Shudi <sdbritman@gmail.comgit config --listgit config --listgit config --global user.name>
2026-01-13 11:45:01 +0200

    add add new item func

--- service_b/app/routes.py
+++ service_b/app/routes.py
@@ -1,6 +1,6 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from storage import get_all, add_item
-
+from schemas import is_valid_coordinates
 
 router = APIRouter()
 
@@ -9,3 +9,12 @@ router = APIRouter()
 def get_all_ip():
     result = get_all()
     return result
+
+
+@router.post("/ip")
+def add_new_item(data: dict):
+    is_valid = is_valid_coordinates(data)
+    if not is_valid:
+        raise HTTPException(status_code=404, detail="Unprocessable Entity")
+    else:
+        return {"message": "ip added successfully"}
