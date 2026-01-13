8fe0ed5f2409d2685be8c8ccb894cf773f0162c1
Shudi <sdbritman@gmail.comgit config --listgit config --listgit config --global user.name>
2026-01-13 09:43:01 +0200

    add 'add item' func

--- service_b/app/storage.py
+++ service_b/app/storage.py
@@ -10,3 +10,12 @@ DB_CONFIG = {
 def get_connection():
     r = redis.Redis(**DB_CONFIG, decode_responses=True)
     return r
+
+
+def add_item(data: dict):
+    r = get_connection()
+    ip = next(iter(data))
+    coords = data[ip]
+    r.hset(f"ip:{ip}", mapping=coords)
+    r.close()
+
