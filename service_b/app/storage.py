22e383ebc87e52dd52d829dc2bfc74a1872fe1e3
Shudi <sdbritman@gmail.comgit config --listgit config --listgit config --global user.name>
2026-01-13 12:13:50 +0200

    remove ip tag before ip address

--- service_b/app/storage.py
+++ service_b/app/storage.py
@@ -16,7 +16,7 @@ def add_item(data: dict):
     r = get_connection()
     ip = next(iter(data))
     coords = data[ip]
-    added_fields = r.hset(f"ip:{ip}", mapping=coords)
+    added_fields = r.hset(ip, mapping=coords)
     r.close()
     if added_fields == 0:
         return "updated"
