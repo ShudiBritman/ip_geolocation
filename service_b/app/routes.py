9c6675fcbd09984d4548f2b73af24b42b51daadf
Shudi <sdbritman@gmail.comgit config --listgit config --listgit config --global user.name>
2026-01-14 16:14:22 +0200

    fix statfulset and coordinate's'

--- service_b/app/routes.py
+++ service_b/app/routes.py
@@ -18,4 +18,4 @@ def add_new_item(data: dict):
         raise HTTPException(status_code=404, detail="Unprocessable Entity")
     else:
         result = add_item(data)
-        return {"message": "ip added successfully", "coordinate": result}
+        return {"message": "ip added successfully", "coordinates": result}
